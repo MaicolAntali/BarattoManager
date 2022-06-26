46de7fd62d820c066b0e94fac5b10124b711df0d
maicolAntali <maicol.antali.ma@gmail.com>
2022-06-26 15:08:13 +0200

    Refactoring CancelOffer.java

--- src/com/barattoManager/ui/customComponents/menu/actions/CancelOffer.java
+++ src/com/barattoManager/ui/customComponents/menu/actions/CancelOffer.java
@@ -1,8 +1,6 @@
 package com.barattoManager.ui.customComponents.menu.actions;
 
-import com.barattoManager.exception.IllegalValuesException;
 import com.barattoManager.exception.NoNodeSelected;
-import com.barattoManager.exception.NullCategoryException;
 import com.barattoManager.manager.ArticleManager;
 import com.barattoManager.model.article.Article;
 import com.barattoManager.model.user.User;
@@ -19,34 +17,35 @@ public class CancelOffer implements MenuAction {
 
 	@Override
 	public void run(RepaintEventHandler repaintEventHandler, User user, Tree tree) {
+		TreeNode[] nodePath;
 		try {
-			// Get the selected node
-			TreeNode[] nodePath = tree.getSelectedPathNode();
+			nodePath = tree.getSelectedPathNode();
+		} catch (NoNodeSelected e) {
+			JOptionPane.showMessageDialog(tree, e.getMessage(), "Errore", JOptionPane.ERROR_MESSAGE);
+			return;
+		}
 
-			var result = JOptionPane.showConfirmDialog(
-					tree,
-					"Sei sicuro di voler cambiare lo stato dell'articolo in: \"Offerta Cancellata\"?",
-					"Conferma Operazione",
-					JOptionPane.YES_NO_OPTION);
+		var result = JOptionPane.showConfirmDialog(
+				tree,
+				"Sei sicuro di voler cambiare lo stato dell'articolo in: \"Offerta Cancellata\"?",
+				"Conferma Operazione",
+				JOptionPane.YES_NO_OPTION);
 
-			if (result == JOptionPane.YES_OPTION) {
-				var article = ArticleManager.getInstance().getArticleById(nodePath[nodePath.length-1].toString());
+		if (result == JOptionPane.YES_OPTION) {
+			var article = ArticleManager.getInstance().getArticleById(nodePath[nodePath.length - 1].toString());
 
-				if (article.isPresent()) {
-					if (article.get().getArticleState() != Article.State.CANCELLED_OFFER) {
-						article.get().changeState(Article.State.CANCELLED_OFFER);
-						repaintEventHandler.fireListeners();
-					}
-					else {
-						throw new IllegalValuesException("Lo stato di questo articolo è gia: \"Offerta Cancellata\"");
-					}
-				}
-				else {
-					throw new  NullCategoryException("Non è stato selezionato un Articolo.\nRiprovare selezionando il nodo madre dell'articolo.");
-				}
+			if (article.isEmpty()) {
+				JOptionPane.showMessageDialog(tree, "Non è stato selezionato un Articolo.\nRiprovare selezionando il nodo madre dell'articolo.", "Errore", JOptionPane.ERROR_MESSAGE);
+				return;
 			}
-		} catch (NoNodeSelected | NullCategoryException | IllegalValuesException ex) {
-			JOptionPane.showMessageDialog(tree, ex.getMessage(), "Errore", JOptionPane.ERROR_MESSAGE);
+
+			if (article.get().getArticleState() == Article.State.CANCELLED_OFFER) {
+				JOptionPane.showMessageDialog(tree, "Lo stato di questo articolo è gia: \"Offerta Cancellata\"", "Errore", JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+
+			article.get().changeState(Article.State.CANCELLED_OFFER);
+			repaintEventHandler.fireListeners();
 		}
 	}
 }
