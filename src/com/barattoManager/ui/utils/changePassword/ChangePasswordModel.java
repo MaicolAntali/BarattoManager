6690df72ace6620c1bd0b5ba343c1fdf5e3e5ada
halima <hlima.damiro@gmail.com>
2022-08-03 11:01:12 +0200

    Change Password MVC

---
+++ src/com/barattoManager/ui/utils/changePassword/ChangePasswordModel.java
@@ -0,0 +1,20 @@
+package com.barattoManager.ui.utils.changePassword;
+
+import com.barattoManager.ui.mvc.base.BaseModel;
+
+public class ChangePasswordModel implements BaseModel {
+
+    private String password;
+
+    public ChangePasswordModel() {
+        this.password = "";
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
