27608eaa75344941c57010e58d2747251b6881bc
maicolAntali <maicol.antali.ma@gmail.com>
2022-08-04 09:07:46 +0200

    Configurator Homepage View

---
+++ src/com/barattoManager/ui/mvc/configurator/homepage/HomepageView.java
@@ -0,0 +1,13 @@
+package com.barattoManager.ui.mvc.configurator.homepage;
+
+import javax.swing.*;
+
+public class HomepageView {
+	private JPanel mainPanel;
+	private JButton configCategoryButton;
+	private JButton configMeetButton;
+	private JButton addNewConfigurator;
+	private JButton showOffer;
+	private JButton loadJsonButton;
+	private JButton loadJsonquestionButton;
+}
