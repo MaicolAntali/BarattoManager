229525a6c73e021fb6992e8e06468d30f9dbc1a7
maicolAntali <maicol.antali.ma@gmail.com>
2022-08-04 14:49:00 +0200

    Add buttons to CategoryEditorView

--- src/com/barattoManager/ui/mvc/configurator/categoryEditor/CategoryEditorView.java
+++ src/com/barattoManager/ui/mvc/configurator/categoryEditor/CategoryEditorView.java
@@ -4,6 +4,9 @@ import javax.swing.*;
 
 public class CategoryEditorView {
 	private JPanel mainPanel;
-	private JPanel centerPanel;
 	private JButton backToHome;
+	private JPanel treePanel;
+	private JButton addRootCategoryButton;
+	private JButton addSubCategoryButton;
+	private JButton addFieldButton;
 }
